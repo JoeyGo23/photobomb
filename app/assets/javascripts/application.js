0428c97ff9b949100313f176b443e73f0e66c3c6
kt32291 <Katharine322@gmail.com>
2014-01-31 22:06:17 -0600

    started adding jquery for client-side validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,64 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+  // $('input').focusin(function() {
+  //   $(this).addClass('select');
+  // });
+  // $('input').focusout(function() {
+  //   $(this).removeClass('select');
+  // });
+
+  // $('.username').keyup(function() {
+  //   if ($(this).val())) {
+  //     $(this).css("border-bottom", "2px solid green");
+  //   }
+  // });
+
+
+
+  $('.email').keyup(function () {
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (re.test($(this).val())) {
+
+        $(this).css("border-bottom", "2px solid green");
+
+      } else {
+
+        $(this).css("border-bottom", "2px solid red");
+      }
+    });
+
+    $('.username').keyup(function () {
+      var value = $(this).val();
+      if ( value.length > 3 ) {
+        $(this).css("border-bottom", "2px solid green");
+        // $(this).css("color", "green");
+      } else {
+        $(this).css("border-bottom", "2px solid red");
+        // $(this).css("color", "red");
+      }
+    });
+
+    $('.password').keyup(function () {
+      var value = $(this).val();
+      if ( value.length > 7 ) {
+        $(this).css("border-bottom", "2px solid green");
+      } else {
+        $(this).css("border-bottom", "2px solid red");
+      }
+    });
+
+    $('.password_confirmation').keyup(function () {
+      var value = $(this).val();
+      var password = $('.password').val();
+      if ( value == password ) {
+        $(this).css("border-bottom", "2px solid green");
+      } else {
+        $(this).css("border-bottom", "2px solid red");
+      }
+    });
+
+});
